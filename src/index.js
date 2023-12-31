e5e7058befb669d648d95989f2be2bed05d2ab4f
Évelyne Lachance <eslachance@gmail.com>
2023-12-31 00:34:06 -0500

    add karaoke routes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router } from 'itty-router';
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
-
-// Export a default object containing event handlers
 export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request, env, ctx) {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
+	async fetch(request, env) {
+		if (!env.__router) {
+			const router = Router();
+			// GET collection index
+			router.get('/', () => new Response('index'));
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
+			// GET item
+			router.get('/get/:id', async ({ params }) => {
+				console.log(params.id);
+				const { results } = await env.DB.prepare(
+					`SELECT * FROM songs WHERE id = ?;`
+				)
+					.bind(parseInt(params.id, 10)).all();
+				console.log(results);
+				return new Response(JSON.stringify(results[0]));
+			});
 
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
+			router.get('/search/:query', async ({ params }) => {
+				const { results } = await env.DB.prepare(
+					`SELECT * FROM songs WHERE hash LIKE ?;`
+				)
+					.bind(`%${params.query}%`).all();
+				console.log(results);
+				return new Response(JSON.stringify(results));
+			});
+
+			router.get('/all', async() => {
+				const { results } = await env.DB.prepare(
+					`SELECT * FROM songs;`
+				).all();
+				console.log(allRows);
+				return new Response(`Karaoke Search: ${JSON.stringify(allRows)}`);
+			})
+
+			// 404 for everything else
+			router.all('*', () => new Response('Not Found.', { status: 404 }));
+			env.__router = router;
 		}
-
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
-
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
+		
+		return env.__router.handle(request);
 	},
 };
